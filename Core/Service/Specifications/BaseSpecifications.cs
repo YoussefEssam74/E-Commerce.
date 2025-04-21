7855f6d474f4ea993fb9d353ab51f4f6168f6230
youssef_74 <youssefessam1293@gmail.com>
2025-04-21 19:19:39 +0200

    Specification

---
+++ Core/Service/Specifications/BaseSpecifications.cs
@@ -0,0 +1,27 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Specifications
+{
+  abstract  public class BaseSpecifications<TEntity, TKey> : ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        protected BaseSpecifications(Expression<Func<TEntity,bool>> CriteriaExpression)
+        {
+            Criteria = CriteriaExpression;
+        }
+      
+        public Expression<Func<TEntity, bool>> Criteria { get; private set; }
+
+        public List<Expression<Func<TEntity, object>>> IncludeExpression { get; } = [];
+                                                                                     // new List<Expression<Func<TEntity, object>>>();
+        protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
+        => IncludeExpression.Add(includeExpression);
+        
+    }
+}
---
+++ InfraStructure/Persistence/SpecificationEvaluator.cs
@@ -0,0 +1,34 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    static class SpecificationEvaluator
+    {
+        // Create Query
+        // _dbContext.Products.Where(P=>P.id=id).Include(P=>P.ProductBrandd).Include(P=>P.ProductType)
+        public static IQueryable<TEntity> CreateQuery<TEntity, TKey>(IQueryable<TEntity> InputQuery, ISpecifications<TEntity, TKey> specifications) where TEntity : BaseEntity<TKey>
+        {
+            var Query = InputQuery;
+            if (specifications.Criteria is not null)
+            {
+                Query = Query.Where(predicate: specifications.Criteria);
+            }
+            if (specifications.IncludeExpression is not null && specifications.IncludeExpression.Count > 0)
+            {
+                //foreach (var exp in specifications. IncludeExpressions) 
+                // Query Query. Include (exp);
+                Query = specifications.IncludeExpression.Aggregate(Query, (CurrentQuery, IncludeExp) => CurrentQuery.Include(IncludeExp));
+
+            }
+                    return Query;
+        }
+    }
+}
+
