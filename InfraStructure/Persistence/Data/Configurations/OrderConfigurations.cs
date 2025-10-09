1c3e8df9d468923fb9ce9b1115ca8e254a95f9dd
youssef_74 <youssefessam1293@gmail.com>
2025-10-09 16:30:06 +0300

    Order Configurations

---
+++ InfraStructure/Persistence/Data/Configurations/OrderConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Order = DomainLayer.Models.OrderModule.Order;
+
+namespace Persistence.Data.Configurations
+{
+    internal class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+
+            builder.Property(o => o.SubTotal)
+                .HasColumnType("decimal(8,2)");
+
+            builder.HasMany(o => o.Items)
+                .WithOne(); 
+
+            builder.HasOne(o => o.DeliveryMethod)
+                   .WithMany()
+                   .HasForeignKey(o => o.DeliveryMethodId);
+
+            builder.OwnsOne(o => o.Address);
+        }
+    }
+}
---
+++ InfraStructure/Persistence/Data/Configurations/OrderItemConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configurations
+{
+    class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+
+            builder.Property(OI => OI.Price)
+                   .HasColumnType("decimal(8,2)");
+
+            builder.OwnsOne(OI => OI.Product);
+        }
+    }
+}
